432c865b5740c2f666bc2efa33d5d00972d46a0a
Sylvain Plante <github@sylvain-plante.me>
2024-07-24 15:52:50 -0400

    Add files via upload
    CiviCRM extension squeleton with angular form and module

---
+++ ang/crmRegister.js
@@ -0,0 +1,4 @@
+(function(angular, $, _) {
+  // Declare a list of dependencies.
+  angular.module('crmRegister', CRM.angRequires('crmRegister'));
+})(angular, CRM.$, CRM._);
---
+++ register.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once 'register.civix.php';
+// phpcs:disable
+use CRM_Register_ExtensionUtil as E;
+// phpcs:enable
+
+/**
+ * Implements hook_civicrm_config().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_config/
+ */
+function register_civicrm_config(&$config): void {
+  _register_civix_civicrm_config($config);
+}
+
+/**
+ * Implements hook_civicrm_install().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_install
+ */
+function register_civicrm_install(): void {
+  _register_civix_civicrm_install();
+}
+
+/**
+ * Implements hook_civicrm_enable().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
+ */
+function register_civicrm_enable(): void {
+  _register_civix_civicrm_enable();
+}
